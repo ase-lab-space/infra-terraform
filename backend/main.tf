6ece998252681562c5aceb711de729f7701407ce
Hiroaki Honda <d@damin.dev>
2023-03-18 01:54:36 +0900

    Block public accesses for s3

--- backend/main.tf
+++ backend/main.tf
@@ -23,3 +23,12 @@ resource "aws_dynamodb_table" "tfstate_lock" {
     type = "S"
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "tfstate" {
+  bucket = aws_s3_bucket.tfstate.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
--- mail_assets/s3/main.tf
+++ mail_assets/s3/main.tf
@@ -33,3 +33,12 @@ data "aws_iam_policy_document" "ase-lab-mail-s3-policy" {
     ]
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "main" {
+  bucket = aws_s3_bucket.main.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
